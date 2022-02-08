d79ebcacbff36e8f51ed33c6b5b338a199b46e88
denisjunior <denisdalmeida04@gmail.com>
2022-02-08 13:06:23 +0100

    my dotnet project

---
+++ Mondote.Dalmeida.FeatureMatching.Console/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text.Json;
+using Mondote.Dalmeida.FeatureMatching;
+
+var imageScenesData = new List<byte[]>();
+foreach (var imagePath in
+         Directory.EnumerateFiles(args[1]))
+{
+    var imageBytes = await File.ReadAllBytesAsync(imagePath);
+    imageScenesData.Add(imageBytes);
+}
+var objectImageData = await
+    File.ReadAllBytesAsync(args[0]);
+var detectObjectInScenesResults = new
+    ObjectDetection().DetectObjectInScenes(objectImageData, imageScenesData);
+
+foreach (var objectDetectionResult in detectObjectInScenesResults)
+{
+    System.Console.WriteLine($"Points:{JsonSerializer.Serialize(objectDetectionResult.Points)}");
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Mondote.Dalmeida.FeatureMatching
+
+lien vers le repo github: https://github.com/denisjunior/Mondote.Dalmeida.FeatureMatching.git
